63a4ab54b5c0b3698b52b07f449b99664864dadb
Fahadk85 <60100766+Fahadk85@users.noreply.github.com>
2023-12-06 16:06:57 -0500

    Created an Events Model class with Events Calendar Business Logic

--- API/Models/Events.cs
+++ API/Models/Events.cs
@@ -7,6 +7,12 @@ namespace API.Models
 {
     public class Events
     {
-        
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description {get; set;}
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public bool IsReminderSet { get; set; }
+        public bool IsRecurring { get; set; }
     }
 }
