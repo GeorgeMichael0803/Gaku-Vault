9e3136ad9a636e1a6aadb25f58388a55c5f40111
Fahadk85 <60100766+Fahadk85@users.noreply.github.com>
2023-12-06 16:36:29 -0500

    CreateEvent and GetEventbyID requests created

--- API/Controller/EventsController.cs
+++ API/Controller/EventsController.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Models;
+using API.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controller
@@ -10,6 +12,27 @@ namespace API.Controller
     [Route("api/[controller]")]
     public class EventsController : ControllerBase
     {
+        private GakuDb _db = new GakuDb();
+
+        [HttpPost]
+        public async Task<IActionResult> CreateEvent(Events events)
+        {
+            await _db.Calendar.AddAsync(events);
+            await _db.SaveChangesAsync();
+            return Ok(events);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetEventbyID(Guid id)
+        {
+            var result = await _db.Calendar.FindAsync(id);
+            await _db.SaveChangesAsync();
+            
+            if (result == null)
+                return NotFound();
+            
+            return Ok(result);
+        }
         
     }
 }
