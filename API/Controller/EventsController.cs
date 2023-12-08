3eb18a08ca8b14762df8d6a1fca2532d14d4a5c3
Fahadk85 <60100766+Fahadk85@users.noreply.github.com>
2023-12-08 17:34:14 -0500

    Added GetAllEevnts and UpdateEvents requests

--- API/Controller/EventsController.cs
+++ API/Controller/EventsController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using API.Models;
 using API.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controller
 {
@@ -31,6 +32,38 @@ namespace API.Controller
             if (result == null)
                 return NotFound();
             
+            return Ok(result);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllEvents()
+        {
+            var result = await _db.Calendar.ToListAsync();
+            await _db.SaveChangesAsync();
+
+            if (result == null)
+                return NotFound();
+
+            return Ok(result);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateEvent(Guid id, Events events)
+        {
+            var result = await _db.Calendar.FindAsync(id);
+
+            if (result == null)
+                return NotFound("Event not Found");
+            
+            result.Title = events.Title;
+            result.Description = events.Description;
+            result.StartTime = events.StartTime;
+            result.EndTime = events.EndTime;
+            result.IsReminderSet = events.IsReminderSet;
+            result.IsRecurring = events.IsRecurring;
+
+            await _db.SaveChangesAsync();
+
             return Ok(result);
         }
         
