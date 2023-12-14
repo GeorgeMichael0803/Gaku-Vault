66989d09c0814e1a054d4cad2cf7a99e2c58cff5
George Michael <michaege@sheridancollege.ca>
2023-12-14 00:26:40 -0500

    Adding frontend v2

---
+++ frontend V2/Tutors/script.js
@@ -0,0 +1,27 @@
+function joinAsTutor() {
+    window.location.href = '/join-as-tutor.html';
+}
+
+function updateTutorProfile() {
+    window.location.href = '/update-tutor-profile.html';
+}
+
+function seeAllTutors() {
+    window.location.href = '/all-tutors.html';
+}
+
+function seeAvailableTutors() {
+    window.location.href = '/available-tutors.html';
+}
+
+function scheduleSession() {
+    window.location.href = '/schedule-session.html';
+}
+
+function getAllTutorSessions() {
+    window.location.href = '/tutor-sessions.html';
+}
+
+function addReview() {
+    window.location.href = '/add-review.html';
+}
